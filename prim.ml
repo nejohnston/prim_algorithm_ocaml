54a233938b55d5d541a3788288296f4b9e1d6d9f
Nicholas Johnston <nicholas.johnston94@gmail.com>
2021-11-14 20:22:40 -0800

    added ugraph and working on prims

--- prim.ml
+++ prim.ml
@@ -1,36 +1,110 @@
 open Ugraph
 
-(* let () =
-  let ic = 
-    if Array.length Sys.argv = 1 then
-      stdin
-    else
-      Stdio.In_channel.create Sys.argv.(1)
-  in
-  Stdio.printf "%s" @@ Stdio.In_channel.fold_lines ~init:() ~f:(fun (v1, v2, w) line ->
-    ) (ic) *)
-(* let split_lines *)
+(*let graph_from_list list = Base.List.map (fun [v1, v2, w] -> add_edge (make_edge (v1, v2, w)) acc)*)
 
-(*let read_file file_name =*)
-(*  let ic = Stdio.In_channel.create file_name in*)
-(*    Stdio.In_channel.fold_lines ~init:Ugraph.empty ~f:(fun acc line ->*)
-(*      (Base.String.split line ~on:(Base.Char.of_string " "))*)
-(*      |> (List.map Base.Int.of_string) |>*)
-(*         Base.List.fold [] (fun acc' [v1, v2, w] -> Ugraph.add_edge (Ugraph.make_edge (v1, v2, w)) acc)) ic*)
+let line_fold acc line =
+    let split = Base.String.split line ~on:(Base.Char.of_string " ") in
+    let int_list = (List.map (fun x -> Base.Int.of_string x) split) in
+    let string_edge = Array.of_list int_list in
+        add_edge(make_edge(string_edge.(0), string_edge.(1), string_edge.(2))) acc;;
 
-(*
+let read_file file_name =
+  let ic = Stdio.In_channel.create file_name in
+    Stdio.In_channel.fold_lines ~init:(empty) ~f:line_fold ic
 
-*)
-let prim start_v g =
-     let visited = ref [start_v];
-     let mst = ref [];
-     while len(visited) < len(mst) do
-        (Ugraph.fold (fun x acc -> Ugraph.fold x (G.map)) g)
+(*let get_least edges mst =*)
+(*    let rec get_least' acc =*)
+(*        match acc, edges with*)
+(*        | (), (v1', v2', w') -> (v1', v2', w')*)
+(*        | (v1, v2, w), (v1', v2', w') when w' < w && !(List.exists (v1', v2', w') mst) then (v1', v2', w') = acc*)
+(*    in*)
+(*    get_least' (G.inf, G.inf, G.inf)*)
+
+(*let get_least edges = List.hd (Base.List.sort edges ~compare:(fun (v1, v2, w) (v1', v2', w') -> Int.compare w w'))*)
+(*(*(List.fold_right ~f:(fun (v1, v2, w) [(v1', v2', w')] ->*)*)
+(*(*                                     if w' > w then (v1, v2, w) else (v1', v2', w')) edges [])*)*)
+
+(*let get_edges visited g =*)
+(*            List.fold_right (fun v acc ->*)
+(*            (*get all neighbours of all the vertices in  visited*)*)
+(*            (List.append (Base.List.map (neighbours v g) (fun (v2, w) -> (v, v2, w))) acc)*)
+(*            ) visited []*)
 
 
+(*let prim start_v g =*)
+(*     let visited = ref [start_v] in*)
+(*     let mst = ref Ugraph.empty in*)
+(*     while List.length(!visited) < List.length(vertices(g)) do*)
+(*        (*folding over visited, folding over the neighbours*)*)
+(*        let edges = (get_edges (!visited) g)*)
+(*        in*)
+(*        (*iterate through edges, finding the least weight*)*)
+(*        let (v1, v2, w) = (get_least edges)*)
+(*        in*)
+(*        mst := (add_edge (make_edge (v1, v2, w)) (!mst))*)
+(*        visited := vertices (!mst)*)
+(*(*        match least with*)*)
+(*(*                   | () -> !(visited)*)*)
+(*(*                   | (v1, v2, w) -> match (List.find_opt v1 !(visited)), (List.find_opt v2 !(visited)) with*)*)
+(*(*                                    | None, None -> G.add v1 !(visited) |> G.add v2 !(visited)*)*)
+(*(*                                    | Some x, None -> G.add v2 !(visited)*)*)
+(*(*                                    | None, Some x -> G.add v1 !(visited)*)*)
+(*        done*)
+
+(*let rec prim' vs tree graph_edges =*)
+(*        let t' = List.filter (fun (v1, v2, _) -> (List.mem v1 vs) && not (List.mem v2 vs)) graph_edges*)
+(*        in*)
+(*        let vs_edges = (List.sort (fun (_, _, w) (_, _, w') -> Int.compare w w') t')*)
+(*        in*)
+(*        let (v1_new, v2_new, w_new) = List.hd vs_edges*)
+(*        in*)
+(*        let new_tree = (v1_new, v2_new, w_new)::tree*)
+(*        in*)
+(*        let new_vs = match (List.mem v1_new vs), (List.mem v2_new vs) with*)
+(*                       | true, false -> v2_new::vs*)
+(*                       | _, _ -> vs*)
+(*        in*)
+(*        if (List.length graph_edges <> 0) then prim' new_vs new_tree (List.tl vs_edges) else new_tree*)
+(* need to get all edges that are in edges parameter, where v1 is in vs, v2 is not in vs, then get smallest weight*)
+
+let rec prim' vs tree graph_edges g_len =
+        let t' = List.filter (fun (v1, v2, _) -> (List.mem v1 vs) && not (List.mem v2 vs)) graph_edges
+        in
+        let (v1_new, v2_new, w_new) = List.hd (List.sort (fun (_, _, w) (_, _, w') -> Int.compare w w') t')
+        in
+        let new_vs = v2_new::vs
+        in
+        let new_tree = (v1_new, v2_new, w_new)::tree
+        in
+        let new_edges = List.filter
+            (fun (v1, v2, w) ->
+            ((v1, v2, w) <> (v1_new, v2_new, w_new)) && ((v2, v1, w) <> (v2_new, v1_new, w_new))) graph_edges
+        in
+        if (List.length new_edges <> 0) then prim' new_vs new_tree new_edges g_len else new_tree
 
 
-  (* (Base.List.fold (fun acc list -> 
+let prim v g =
+    prim' [v] [] (edges g) (List.length (vertices(g)))
+
+let rec rec_print list =
+    match list with
+    | [] -> ()
+    | (v1, v2, w) :: t -> Stdio.printf "<%d, %d, %d> " v1 v2 w; rec_print t
+
+let weights list =
+    let rec aux list acc =
     match list with
     | [] -> acc
-    | [v1, v2, w] -> (Ugraph.add_edge (Ugraph.make_edge (v1 v2 w)) Ugraph.empty acc)) ) *)
+    | (_, _, w) :: t -> aux t (acc+w)
+    in
+    aux list 0
+
+let () =
+    let g = read_file "h.txt"
+    in
+    let vs = prim 1 g
+    in
+    Stdio.printf "minimum weight: %d\n" (weights(vs));
+    rec_print vs;
+
+
--- ugraph.ml
+++ ugraph.ml
@@ -1,22 +1,23 @@
 module G = Map.Make(Int)
 
-type edge = Edge of int * int * int
+type edge = (int * int * int)
 type t = (int G.t) G.t
 
 let inf = -1
 
 let empty = G.empty
 
-let make_edge (v1, v2, w) = Edge(v1, v2, w)
+let make_edge (v1, v2, w) = (v1, v2, w)
 
-let add_edge (Edge(v1, v2, w)) g =
+(* adds edge to the  current graph *)
+let add_edge' (v1, v2, w) g =
     match (G.find_opt v1 g) with
-    | None when g = empty -> G.empty |> (G.add v1 (G.empty |> G.add v2 w))
-    | None -> g |> (G.add v1 (G.empty |> G.add v2 w))
-    | Some x ->
-        match (G.find_opt v2 x) with
-        | None -> g |> (G.add v1 (G.empty |> G.add v2 w))
-        | _ -> g
+    | None -> g |> (G.add v1 (empty |> G.add v2 w))
+    | Some x when not (G.mem v2 x) -> (G.add v1 (x |> G.add v2 w) g)
+    | Some _ -> g
+
+let add_edge (v1, v2, w) g =
+    g |> add_edge' (v1, v2, w) |> add_edge' (v2, v1, w)
 
 (*vertices need to be mapped to an array, along with the arrays that they have
     we also need to check if they're already in the accumulator*)
@@ -26,7 +27,15 @@ let vertices g =
 
 (*fold over outer map, enter fun, fold over inner map, finnd v2, add to acc*)
 let neighbours v g =
-    (G.fold (fun v1 v acc -> G.fold (fun v2 w acc' -> (v2, w)::acc') v acc) g [])
+    match (G.find_opt v g) with
+    | None -> []
+    | Some x -> (G.bindings x)
+
+(*    let n = (G.find v g) in*)
+(*    G.fold (fun v2' w' acc -> (v2', w')::acc) n []*)
+(*    match (G.find_opt v g) with*)
+(*    | None -> []*)
+(*    | Some x -> G.fold (fun v2' w' acc -> (v2', w')::acc) x []*)
 
 (*fold over outer map, enter fun, fold over inner map, create edge, add to acc*)
 let edges g =
@@ -34,8 +43,15 @@ let edges g =
 
 let weight v1 v2 g =
     try
-        let w = G.find v1 g
+(*        (G.find v1 g)*)
+        let vertex_array = G.find v1 g
         in
-        G.find v2 w
+        G.find v2 vertex_array
     with
         Not_found -> inf
+
+
+(*#directory "_build";;*)
+(*#load "ugraph.cmo";;*)
+(*#show Ugraph;;*)
+(*open Ugraph;;*)
